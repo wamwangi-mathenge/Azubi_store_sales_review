0392774c4948f1ec36694931642ce9167401d15b
Brian <wamwangimathenge@gmail.com>
2023-09-02 15:04:02 +0300

    Add f string concatenation to outputs

--- store.py
+++ store.py
@@ -7,7 +7,7 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
 # Calculate the total price average for all products
 
 average_price = sum(prices) / len(prices)
-print(average_price)
+print(f"Average Price: {average_price}")
 
 print()
 
@@ -16,7 +16,7 @@ for price in prices:
     new_price = int(price) - 5
     new_price_list.append(new_price)
     
-print(new_price_list)
+print(f"New Price List: {new_price_list}")
 
 print()
 
@@ -29,13 +29,13 @@ quantities_array = np.array(last_week)
 
 total_revenue = np.dot(prices_array, quantities_array)
 
-print(total_revenue)
+print(f"Total revenue: {total_revenue}")
 
 print()
 
 # Calculate the average daily revenue generated from the products
 
 average_daily_revenue = total_revenue / 7
-print(average_daily_revenue)
+print(f"Average Daily Revenue: {average_daily_revenue}")
 
 print()
