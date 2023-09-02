d2b1d17b6ac8b412edefb1d237a2330100a821d7
Brian <wamwangimathenge@gmail.com>
2023-09-02 13:50:07 +0300

    Calculate the total price average

---
+++ store.py
@@ -0,0 +1,10 @@
+products = ["Sankofa Foods", "Jamestown Coffee", "Bioko Chocolate", "Blue Skies Ice Cream", "Fair Afric Chocolate", "Kawa Moka Coffee", "Aphro Spirit", "Mensado Bissap", "Voltic"]
+
+prices = [30, 25, 40, 20, 20, 35, 45, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
+
+# Calculate the total price average for all products
+
+average_price = sum(prices) / len(prices)
+print(average_price)
